ede8b8ff825e6704f01d68b4183a93ed4f919899
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-22 22:11:52 +0500

    Chapter 2 Variables for String

---
+++ Chapter 2 Variables for String/app.js
@@ -0,0 +1,23 @@
+// var str = "Saylani"
+// console.log(str);
+var username ;
+var myName = "MustafaShahzad";
+var message = "Hello World";
+alert(message)
+
+
+
+var stdName = "Mustafa";
+var stdAge = 16;
+var stdCountry = "Pakistan"
+
+
+alert(stdName);
+alert(stdAge);
+alert(stdCountry);
+
+var content = "Maine HTML content JS se Likh lia" ;
+document.writeln(content)
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design)
