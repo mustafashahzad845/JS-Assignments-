9e81246aca18c26764cb21f47f7dee9251c61677
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-22 22:26:07 +0500

    Chapter 3 Variables for Number

---
+++ Chapter 3 Variables for Number/app.js
@@ -0,0 +1,16 @@
+var age = "I am 16 years old";
+// alert(age)
+
+
+var birthYear = 2010;
+// alert(birthYear);
+
+document.writeln(birthYear + "<br>" + "Maine jo content likha hai uski data type number hai" + "<br>")
+document.write("<br>")
+
+
+var visitorsName = "Ghulam Mustafa";
+var productTitle =  "T-Shirt";
+var quantity = 15 + "products" 
+
+document.writeln(`${visitorsName} , ${productTitle} , ${quantity}`)
