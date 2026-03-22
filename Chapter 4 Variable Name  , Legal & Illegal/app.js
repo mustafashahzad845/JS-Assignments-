f1dd8ec2ea017ca877ec031dfa7ce86652d7f863
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-22 22:38:49 +0500

    Chapter 4 Variable Name  , Legal & Illegal

---
+++ Chapter 4 Variable Name  , Legal & Illegal/app.js
@@ -0,0 +1,14 @@
+var userName = "Ghulam Mustafa";
+var user Name = "Ghulam Mustafa" ;
+var user$Name = "Ghulam Mustafa";
+var user_Name = "Ghulam Mustafa";
+var userName1 = "Ghulam Mustafa"
+var user!Name = "Ghulam Mustafa"
+var user#Name = "Ghulam Mustafa"
+var user+Name = "Ghulam Mustafa"
+var userName1 = "Ghulam Mustafa"
+var 1userName = "Ghulam Mustafa"
+
+
+
+alert()
