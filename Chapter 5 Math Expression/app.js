24baa85af08c4cf93934f7f87376003181203fe4
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-24 00:13:23 +0500

    Chapter 5 Math Expression

---
+++ Chapter 5 Math Expression/app.js
@@ -0,0 +1,112 @@
+var num1 = 5;
+var num2 = 3;
+var ans = num1 + num2;
+var ans2 = num1 - num2;
+var ans3 = num1 * num2;
+var ans4 = num1 / num2;
+var ans5 = num1 % num2
+document.writeln("The sum of " + num1 + " or " + num2 + " is " + ans + "<br>")
+document.writeln("<br>")
+document.writeln("The difference of " + num1 + " or " + num2 + " is " + ans2 + "<br>")
+document.writeln("<br>")
+document.writeln("The Multiplication of " + num1 + " or " + num2 + " is " + ans3 + "<br>")
+document.writeln("<br>")
+document.writeln("The division of " + num1 + " or " + num2 + " is " + ans4 + "<br>")
+document.writeln("<br>")
+document.writeln("The modulus of " + num1 + " or " + num2 + " is " + ans5 + "<br>")
+document.writeln("<br>")
+
+
+document.writeln("<br>")
+document.writeln("<h1>" + "Table of 2" + "</h1>")
+document.writeln("2 x 1 = " + 2 * 1 + "<br>")
+document.writeln("2 x 2 = " + 2 * 2 + "<br>")
+document.writeln("2 x 3 = " + 2 * 3 + "<br>")
+document.writeln("2 x 4 = " + 2 * 4 + "<br>")
+document.writeln("2 x 5 = " + 2 * 5 + "<br>")
+document.writeln("2 x 6 = " + 2 * 6 + "<br>")
+document.writeln("2 x 7 = " + 2 * 7 + "<br>")
+document.writeln("2 x 8 = " + 2 * 8 + "<br>")
+document.writeln("2 x 9 = " + 2 * 9 + "<br>")
+document.writeln("2 x 10 = " + 2 * 10 + "<br>")
+document.writeln("2 x 11 = " + 2 * 11 + "<br>")
+document.writeln("2 x 12 = " + 2 * 12 + "<br>")
+
+// for(var i = 1 ; i <= 100 ; i++){
+// document.writeln("4 x " + i + "=" + 4 * i + "<br>")
+// }
+
+
+var priceOfItem1 = 650;
+var quantityOfItem1 = 3;
+var priceOfItem2 = 800;
+var quantityOfItem2 = 5;
+var priceOfItem3 = 1000;
+var quantityOfItem3 = 10;
+
+var item1KiQimat = priceOfItem1 * quantityOfItem1
+var item2KiQimat = priceOfItem2 * quantityOfItem2
+var item3KiQimat = priceOfItem3 * quantityOfItem3
+
+var totalCost = item1KiQimat + item2KiQimat + item3KiQimat
+console.log(item1KiQimat, "item1KiQimat")
+console.log(item2KiQimat, "item2KiQimat")
+console.log(item3KiQimat, "item3KiQimat")
+console.log(totalCost, "totalCost");
+
+
+
+
+
+var obtainedMarks = 500;
+var totalMarks = 650;
+
+var percentage = Math.floor((obtainedMarks / totalMarks) * 100)
+
+document.writeln("<br>")
+
+
+document.writeln("Obtained Marks: ", obtainedMarks, "<br>")
+document.writeln("<br>")
+document.writeln("Total Marks: ", totalMarks, "<br>")
+document.writeln("<br>")
+document.writeln("Percentage: ", percentage, "%", "<br>")
+document.writeln("<br>")
+
+
+
+document.writeln("<br>")
+
+var currentYear = 2026;
+var birthYear = 2010;
+var age = currentYear - birthYear;
+
+document.writeln(currentYear, " currentYear", "<br>")
+document.writeln("<br>")
+document.writeln(birthYear, " birthYear", "<br>")
+document.writeln("<br>")
+document.writeln(age, " age", "<br>")
+document.writeln("<br>")
+
+document.writeln("<br>")
+// Properties of A circle
+var circleRadius = 20
+var circleCicumference = (2 * 3.142) * circleRadius
+var areaCircle = 3.142 * circleRadius * circleRadius
+
+
+document.writeln(circleRadius , "circleRadius" , "<br>")
+document.writeln(circleCicumference , "circleCicumference" , "<br>")
+document.writeln(areaCircle , "areaCircle" , "<br>")
+
+
+
+// Life time Calculator
+var favoriteSnack = "birayni";
+var currentAge = 15;
+var maxAge = 65;
+var estimateAmount = 3;
+
+
+
+
