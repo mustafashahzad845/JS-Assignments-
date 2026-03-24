08fe87c68c59a6a1debf149b6b2c6946523849d4
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-24 23:56:29 +0500

    Chapter 10 - 14 If , Nested If , gates

---
+++ Chapter 10 - 14 If , Nested If , gates/app.js
@@ -0,0 +1,158 @@
+// var gender = prompt("Enter your gender").trim().toLowerCase()
+
+
+// if(gender === "male"){
+// console.log("Head Office Campus");
+
+// }else if(gender === "female"){
+//     console.log("Aliabad Campus");
+
+// }else{
+//     console.log(`Invalid Gender "${gender}" is not a correct gender`);
+
+// }
+
+
+
+
+// var city = prompt("Enter Your City")
+
+// if(city === "Karachi" || city === "karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+
+
+// Traffic Signal 
+// var trafficSignalColor = prompt("Bhai Traffic signal ka color kia horaha hai").trim().toLowerCase()
+
+// if(trafficSignalColor === "red"){
+//     alert("Must Stop")
+// }else if(trafficSignalColor === "green"){
+//     alert("Move now")
+// }else if(trafficSignalColor === "yellow"){
+//     alert("Ready to move")
+// }else{
+//     alert("Invalid Color")
+// }
+
+
+
+
+// Mark sheet
+// var html = +prompt("Enter HTML Marks")
+// var css = +prompt("Enter css Marks")
+// var js = +prompt("Enter js Marks")
+// var github = +prompt("Enter github Marks")
+// var bootstrap = +prompt("Enter bootstrap Marks")
+
+// var totalMarks = 500;
+// var obtainedMarks = html + css + js + github + bootstrap
+// var percentage = (obtainedMarks / totalMarks) * 100
+
+// percentage = percentage.toFixed(2)
+
+// console.log(html + " html");
+// console.log(css + " css");
+// console.log(js + " js");
+// console.log(github + " github");
+// console.log(bootstrap + " bootstrap");
+// console.log(obtainedMarks + " obtainedMarks");
+// console.log(totalMarks + " totalMarks");
+// console.log(percentage + "%");
+
+
+// var grade = "Invalid Grade"
+
+// if (percentage >= 80 && percentage <= 100) {
+//     grade = "A+"
+// } else if (percentage < 80 && percentage >= 70) {
+//     grade = "A"
+// } else if (percentage < 70 && percentage >= 60) {
+//     grade = "B"
+// } else if (percentage < 60 && percentage >= 50) {
+//     grade = "C"
+// }else if(percentage < 50 && percentage >= 40){
+//     grade = "D"
+// }else if(percentage < 40){
+//     grade = "Fail"
+// }
+
+
+
+// console.log("Grade:", grade);
+
+
+
+
+
+// Password Checker
+// var signupPassword = prompt("Enter Signup Password").trim()
+// var loginPassword = prompt("Enter Login Password").trim()
+
+
+// console.log(signupPassword);
+// console.log(loginPassword);
+
+// if (signupPassword == "" || loginPassword == "") {
+//     alert("Please enter your password")
+// } else if (signupPassword === loginPassword) {
+//     alert("Correct Password")
+// } else {
+//     alert("Invalid Password")
+// }
+
+
+
+
+// var signupEmail = "mustafa@gmail.com";
+// var signupPassword = "mustafa123";
+// var mobileNumber = 923213880304;
+// var otp = 123456;
+
+
+// var loginEmail = prompt("Enter login Email")
+
+
+
+// if (signupEmail === loginEmail) {
+//     console.log("Email Match");
+
+
+//     var loginPassword = prompt("Enter login Passoword")
+
+//     if (signupPassword === loginPassword) {
+//         console.log("Password Match");
+
+//         var confirmMobileNumber = +prompt("Enter your confirm Mobile Number")
+
+
+//         if (mobileNumber === confirmMobileNumber) {
+//             console.log("Correct Mobile Number");
+//             var OneTimePassword = +prompt("Enter your One Time Password")
+
+//             if (otp === OneTimePassword) {
+//                 alert("Login Successfully 😊");
+
+//             } else {
+//                 console.log("Invalid OTP");
+
+//             }
+
+//         } else {
+//             console.log("Invalid Mobile Number");
+
+//         }
+//     } else {
+//         console.log("Incorrect Password");
+
+//     }
+
+// } else {
+//     console.log("Invalid Email");
+
+// }
+
+
+
+
