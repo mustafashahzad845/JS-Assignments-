d1e44c72c643a1fa464075bdeb1d718f451b9f25
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-24 17:27:06 +0500

    Chapter 8 & 9 Concate and Prompt

---
+++ Chapter 8 & 9 Concate and Prompt/app.js
@@ -0,0 +1,29 @@
+// var name = "Mustafa";
+// var fatherName = "Shahzad";
+// var fullName = name + " " +fatherName;
+// console.log(fullName);
+
+
+
+// console.log( 5 + "str");
+
+
+
+
+// Prompt
+
+// var user = prompt("Enter your name" , "Mustafa")
+
+
+
+// if(user.trim() == ""){
+// console.log("Kuch likho tou sahi")
+// }else{
+//     console.log(user);
+// }
+
+
+
+var num1 = prompt("Enter num1")
+var num2 = prompt("Enter num2")
+console.log(num1 - num2 , typeof num1 , typeof num2);
