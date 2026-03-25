ae5749ce5c963f6400e52674c192a16f7c2097b8
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-25 22:37:59 +0500

    Chapter 18 For Loop

---
+++ Chapter 18 For Loop/app.js
@@ -0,0 +1,61 @@
+// for(initialization , condition , increment/decrement){
+
+// }
+
+
+// for(var i = 1 ; i <= 10 ; i++){
+// console.log(i);
+
+// }
+
+
+// var tableNumber = +prompt("Enter table number");
+// var tableStartingNumber = +prompt("Enter table starting number")
+// var tableEndingNumber = +prompt("Enter table ending number")
+
+// for(var i = tableStartingNumber ; i <= tableEndingNumber ; i++){
+// console.log(`${tableNumber} x ${i} = ${tableNumber * i}`);
+
+// }
+
+
+
+
+
+// var tableNumber = +prompt("Enter table number")
+// var tableStartingNumber = +prompt("Enter table starting number")
+// var tableEndingNumber = +prompt("Enter table Ending number")
+
+// for(var i = tableStartingNumber ; i <= tableEndingNumber ; i++){
+// console.log(`${tableNumber} x ${i} = ${i * tableNumber}`);
+
+// }
+
+
+
+// var startingNumber = +prompt("Enter starting Number");
+// var endingNumber = +prompt("Enter ending Number");
+// var evenOdd = prompt("Which type of number you want  even or odd").trim().toLowerCase()
+
+// for(var i = startingNumber ; i <= endingNumber ; i++){
+// if(evenOdd === "even"){
+// if(i % 2 === 0){
+// console.log(i);
+
+// }
+// }else if(evenOdd === "odd"){
+// if(i % 2 !== 0){
+// console.log(i);
+
+// }
+// }else if(evenOdd === ""){
+// alert("Please fill the input field")
+// }
+// }
+
+
+
+// backward loop 
+for(var i = 100 ; i >= 1 ; i--){
+console.log(i);
+}
