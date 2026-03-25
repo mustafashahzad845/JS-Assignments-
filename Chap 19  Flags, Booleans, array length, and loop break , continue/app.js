d9e303edceccaf7869f61907e3fbe449253e02f6
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-03-26 02:38:05 +0500

    Chap 19  Flags, Booleans, array length, and loop break , continue

---
+++ Chap 19  Flags, Booleans, array length, and loop break , continue/app.js
@@ -0,0 +1,40 @@
+// var cities = ["Karachi", "Lahore", "Hyderabad", "Quetta"]
+// var isCheck = false
+// var userCity = prompt("Enter your city").trim()
+
+// for(var i = 0 ; i < cities.length ; i++){
+//     console.log(cities[i]);   
+
+// if(userCity == cities[i]){
+// isCheck = true
+// alert("Apki city hai 😊")
+// break
+// }
+
+// }
+
+
+// if(isCheck == false){
+// alert("Apki city nahi hai 🚫")
+// }
+
+
+
+
+// if (cities.indexOf(userCity) !== -1) {
+//     alert("Apki city hai 😊")
+// } else if (userCity == "") {
+//     alert("Please Enter your city")
+// } else {
+//     alert("Apki city nahi hai 🚫")
+// }
+
+
+for(var i = 1 ;i <= 40 ; i++){
+if(i == 5 || i == 10 || i == 15 || i == 20 || i == 25 || i == 30 || i == 35 || i == 40){
+// break
+continue
+}
+console.log(i);
+
+}
